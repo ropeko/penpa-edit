8b14ca464215317696910a0cebf91d43447abd32
ropeko <ropeko@yahoo.de>
2020-10-28 15:54:44 +0100

    Revert "Revert "One number candidate""
    This reverts commit 93aae100ad77642cde8694378cd284b85a4a9e89.

--- docs/js/class_square.js
+++ docs/js/class_square.js
@@ -1200,23 +1200,12 @@ class Puzzle_square extends Puzzle {
                     break;
                 case "7": //sudoku
                     this.draw_numbercircle(pu, i, p_x, p_y, 0.42);
-                    var sum = 0,
-                        pos = 0;
+                    // Different to the original penpa-edit: if only one number is entered as candidate, it won't assume that it is the correct number.
+                    // For Sudoku it happens often that only one candidate number is in a cell (and maybe other candidates will be added later).
+                    set_font_style(this.ctx, 0.3 * this.size.toString(10), this[pu].number[i][1]);
                     for (var j = 0; j < 9; j++) {
                         if (this[pu].number[i][0][j] === 1) {
-                            sum += 1;
-                            pos = j;
-                        }
-                    }
-                    if (sum === 1) {
-                        set_font_style(this.ctx, 0.7 * this.size.toString(10), this[pu].number[i][1]);
-                        this.ctx.text((pos + 1).toString(), p_x, p_y + 0.06 * this.size, this.size * 0.8);
-                    } else {
-                        set_font_style(this.ctx, 0.3 * this.size.toString(10), this[pu].number[i][1]);
-                        for (var j = 0; j < 9; j++) {
-                            if (this[pu].number[i][0][j] === 1) {
-                                this.ctx.text((j + 1).toString(), p_x + ((j % 3 - 1) * 0.28) * this.size, p_y + (((j / 3 | 0) - 1) * 0.28 + 0.02) * this.size);
-                            }
+                            this.ctx.text((j + 1).toString(), p_x + ((j % 3 - 1) * 0.28) * this.size, p_y + (((j / 3 | 0) - 1) * 0.28 + 0.02) * this.size);
                         }
                     }
                     break;
